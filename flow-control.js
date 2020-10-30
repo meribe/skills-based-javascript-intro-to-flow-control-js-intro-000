01add4e7a94c751c3f1322eb01404846f71891a4
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-10-30 15:49:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,29 +1,25 @@
-function basicTeenager(age) {
-  var age
-  if  (age >= 13 && age =< 19){
-  console.log("You are a teenager!")}
-  else {
-    return
-  }
+function basicTeenager(age) { 
+  if  (basicTeenager(age) >= 13 && age =< 19){
+  return("You are a teenager!")}
+  
 }
 
 function teenager(age) {
-  if (age> 13 && age< 19) {
+  if (teenager> 13 && age< 19) {
     return("You are a teenager!")}
   else {
     return ("You are not a teenager!") }
   }
-
   }
 
 function ageChecker(age) {
   if (age> 13 && age< 19) {
-  return ("You are a teenager")
+    return ("You are a teenager")
 } else if {
    (age =< 12 )
-  return ("You are a kid")
+   return ("You are a kid")
 } else {
-  return ("You are a grownup")
+    return ("You are a grownup")
 }
 
 function ternaryTeenager(age) {
